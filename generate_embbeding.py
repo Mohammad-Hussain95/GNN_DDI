8ea048bb501d1b94d59b8007ec138977f1eab61e
hussain <34011132+Mohammad-Hussain95@users.noreply.github.com>
2022-06-25 15:49:51 +0430

    Update generate_embbeding.py

--- generate_embbeding.py
+++ generate_embbeding.py
@@ -1,4 +1,4 @@
-!python "/DDI/GNN/main.py" --input "/content/drive/MyDrive/DDI/data5" --features "/content/drive/MyDrive/DDI/data5/featuers_m1.txt" --epoch 1 --dimensions 32
-!python "/DDI/GNN/main.py" --input "/content/drive/MyDrive/DDI/data5" --features "/content/drive/MyDrive/DDI/data5/featuers_m2.txt" --epoch 1 --dimensions 32
-!python "/DDI/GNN/main.py" --input "/content/drive/MyDrive/DDI/data5" --features "/content/drive/MyDrive/DDI/data5/featuers_m3.txt" --epoch 1 --dimensions 32
-!python "/DDI/GNN/main.py" --input "/content/drive/MyDrive/DDI/data5" --features "/content/drive/MyDrive/DDI/data5/featuers_m4.txt" --epoch 1 --dimensions 32
+!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m1.txt" --epoch 1 --dimensions 32
+!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m2.txt" --epoch 1 --dimensions 32
+!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m3.txt" --epoch 1 --dimensions 32
+!python "/GNN/main.py" --input "/DDI/data5" --features "/DDI/data5/featuers_m4.txt" --epoch 1 --dimensions 32
